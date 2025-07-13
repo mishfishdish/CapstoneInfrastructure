318f2823b06512b53f342c226de57aaa872f867a
mishfishdish <mishfishhh@gmail.com>
2025-07-13 23:00:00 +1000

    [terraform] fix key pair

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,11 @@
+resource "tls_private_key" "ec2" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "aws_key_pair" "deployer" {
   key_name   = var.key_name
-  public_key = var.public_key
+  public_key = tls_private_key.ec2.public_key_openssh
 
   lifecycle {
     ignore_changes = [public_key]
